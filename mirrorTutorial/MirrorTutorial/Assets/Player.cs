43a21a1c69c7423151a68a353250b0467b937fa8
turbolek <turbolek@gmail.com>
2020-06-04 17:04:55 +0200

    Added players moving forward on space down

---
+++ mirrorTutorial/MirrorTutorial/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField]
+    private Vector3 _movement = new Vector3();
+
+    [Client]
+    private void Update()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        if (!Input.GetKeyDown(KeyCode.Space))
+        {
+            return;
+        }
+
+        transform.Translate(_movement);
+    }
+
+}
