56e03e9a33363f8399e7f949ce80d4a3694b6914
turbolek <turbolek@gmail.com>
2020-06-04 18:12:48 +0200

    Spawning common pet for all clients

---
+++ mirrorTutorial/MirrorTutorial/Assets/MyNetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    [SerializeField]
+    private GameObject _petPrefab;
+    [SerializeField]
+    private Transform _petParent;
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        GameObject petObject = Instantiate(_petPrefab, _petParent.position, _petParent.rotation);
+        NetworkServer.Spawn(petObject);
+    }
+}
