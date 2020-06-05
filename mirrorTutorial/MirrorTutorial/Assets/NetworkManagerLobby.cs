190b67894d5ee79dd40bc8c3ea7411e58b08a2e7
turbolek <turbolek@gmail.com>
2020-06-05 16:39:56 +0200

    Added lobby scene

---
+++ mirrorTutorial/MirrorTutorial/Assets/NetworkManagerLobby.cs
@@ -0,0 +1,61 @@
+﻿using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkManagerLobby : NetworkManager
+{
+    [Scene]
+    [SerializeField]
+    private string _menuSceneName;
+
+    [Header("Room")]
+    [SerializeField]
+    private NetworkRoomPlayer _roomPlayerPrefab;
+
+    public static event Action ClientConnected;
+    public static event Action ClientDisconnected;
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        ClientConnected?.Invoke();
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        base.OnClientDisconnect(conn);
+        ClientDisconnected?.Invoke();
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        if (numPlayers >= maxConnections)
+        {
+            conn.Disconnect();
+            return;
+        }
+
+        Scene menuScene = SceneManager.GetSceneByName(_menuSceneName);
+        //Prevents player from joining to the game that has already started. This is what the tutorial does, but we don't want it in final version of the HUB game. TODO: delete after the tutorial
+        if (menuScene == null || !menuScene.isLoaded)
+        {
+            conn.Disconnect();
+            return;
+        }
+
+        base.OnServerConnect(conn);
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        Scene menuScene = SceneManager.GetSceneByName(_menuSceneName);
+        if (menuScene != null && menuScene.isLoaded)
+        {
+            GameObject roomPlayer = Instantiate(_roomPlayerPrefab);
+            NetworkServer.AddPlayerForConnection(conn, roomPlayer);
+        }
+    }
+}
---
+++ mirrorTutorial/MirrorTutorial/Assets/PlayerNamePanel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerNamePanel : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField _nameInput;
+    [SerializeField]
+    private Button _continueButton;
+    [SerializeField]
+    private Button _saveNameButton;
+
+    public static string DisplayName { get; private set; }
+
+    private const string PLAYER_NAME_KEY = "PlayerName";
+
+    private void Start()
+    {
+        SetContinueButtonState(false);
+        SetupInputField();
+    }
+
+    private void SetupInputField()
+    {
+        if (PlayerPrefs.HasKey(PLAYER_NAME_KEY))
+        {
+            _nameInput.text = PlayerPrefs.GetString(PLAYER_NAME_KEY, "Player");
+        }
+
+        SavePlayerName(_nameInput.text);
+    }
+
+    private void SavePlayerName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            return;
+        }
+
+        PlayerPrefs.SetString(PLAYER_NAME_KEY, name);
+        SetContinueButtonState(true);
+    }
+
+    private void SetContinueButtonState(bool enabled)
+    {
+        _continueButton.interactable = enabled;
+    }
+
+
+
+}
